ac7aeb7677de7d87674c215662d7a6c236d7ea93
ichiroc <nobitahouse@gmail.com>
2019-01-06 06:46:23 +0900

    vuetify でレイアウトしてみる

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="editor">
-    <h1>エディター画面</h1>
-    <span>{{user.displayName}}</span>
-    <button @click='logout'>ログアウト</button>
-    <div class="editorWrapper">
-    <div class="memoListWrapper">
-      <button class="addMemoBtn" @click="addMemo">メモの追加</button>
-      <button class='deleteMemoBtn' @click='deleteMemo' v-if='memos.length > 1'>選択中のメモの削除</button>
-      <button class='saveMemmoBtn' @click='saveMemos'>メモの保存</button>
-      <div class='memoList' v-for='(memo, index) in memos' @click='selectMemo(index)' :data-selected='index == selectedIndex'>
-        <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
-        <span>{{ formattedTime(memo.updatedAt) }}</span>
-      </div>
-    </div>
-      <textarea class="markdown" @keyup='onMemoUpdated' v-model="memos[selectedIndex].markdown"></textarea>
-      <div class="preview markdown-body" v-html="preview()"></div>
-    </div>
-  </div>
+<!-- color=success などを使うには v-app で囲う必要がある -->
+<v-app class="editor">
+  <h1>エディター画面</h1>
+  <v-container fluid class='header-container'>
+    <v-layout row justify-space-between>
+      <v-flex xs1>
+        <span>{{user.displayName}}</span>
+      </v-flex>
+      <v-flex xs1>
+        <v-btn @click='logout'>ログアウト</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout class="editorWrapper">
+      <v-flex class="memoListWrapper" xs2 px-1>
+        <v-btn color='success' class='addMemoBtn' @click='addMemo'>メモの追加</v-btn>
+        <v-btn color='error' class='deleteMemoBtn' @click='deleteMemo' v-if='memos.length > 1'>選択中のメモの削除</v-btn>
+        <div class='memoList' v-for='(memo, index) in memos' @click='selectMemo(index)' :data-selected='index == selectedIndex'>
+          <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+          <span>{{ formattedTime(memo.updatedAt) }}</span>
+        </div>
+      </v-flex>
+      <v-flex xs5 px-1>
+        <textarea class="markdown" @keyup='onMemoUpdated' v-model="memos[selectedIndex].markdown"></textarea>
+      </v-flex>
+      <v-flex xs5 px-1>
+        <div class="preview markdown-body" v-html="preview()"></div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</v-app>
 </template>
 
 <script>
@@ -70,9 +82,9 @@ export default {
       return this.memos[this.selectedIndex]
     },
     onMemoUpdated: function(){
-      // new Date
+      // オブジェクトはfirebaseに保存できないので、プリミティブな値を保存
       this.memos[this.selectedIndex].updatedAt = this.currentUnixTime()
-      this.saveMmoes()
+      this.saveMemos()
     },
     logout: function(){
       firebase.auth().signOut()
@@ -93,8 +105,6 @@ export default {
       }
     },
     saveMemos: function() {
-      console.log('save!')
-      console.log(this.memos)
       firebase.database()
         .ref(`memos/${this.user.uid}`)
         .set(this.memos);
@@ -110,16 +120,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.header-container {
+    height: 50px;
+}
 .editorWrapper{
-  display: flex;
+    display: flex;
 }
 .markdown {
-  width: 40%;
-  height: 500px;
+    height: 500px;
+    width: 100%;
 }
 .memoListWrapper {
-  width: 20%;
-  border-top: 1px solid #000;
+    border-top: 1px solid #000;
 }
 .memoList{
     padding: 10px;
@@ -127,26 +139,25 @@ export default {
     text-align: left;
     border-bottom: 1px solid #000;
     &:nth-child(even) {
-      background-color: #ccc;
+        background-color: #ccc;
     }
     &[data-selected="true"] {
-      background-color: #ccf;
+        background-color: #ccf;
     }
 }
 .addMemoBtn {
     margin-top: 20px;
 }
 .deleteMemoBtn {
-  margin: 10px;
+    margin: 10px;
 }
 .memoTitle {
-  height: 1.5em;
-  margin: 0;
-  white-space: nowrap;
-  overflow: hidden;
+    height: 1.5em;
+    margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
 }
 .preview {
-  width: 40%;
-  text-align: left;
+    text-align: left;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import 'shitajicss/docs/css/shitaji.min.css'
 import './scss/style.scss'
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
+
+Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
