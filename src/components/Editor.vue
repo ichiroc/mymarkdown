f0f9db134711585e540e86dbd306031235442df9
ichiroc <nobitahouse@gmail.com>
2019-01-05 11:33:25 +0900

    firebase に保存するように。起動時にそれを読み込むように

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -10,6 +10,7 @@
       </div>
       <button class="addMemoBtn" @click="addMemo">メモの追加</button>
       <button class='deleteMemoBtn' @click='deleteMemo' v-if='memos.length > 1'>洗濯中のメモの削除</button>
+      <button class='saveMemmoBtn' @click='saveMemos'>メモの保存</button>
     </div>
       <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
       <div class="preview" v-html="preview()"></div>
@@ -33,6 +34,16 @@ export default {
       selectedIndex: 0
     };
   },
+  created: function() {
+    firebase.database()
+      .ref(`memos/${this.user.uid}`)
+      .once('value')
+      .then(result => {
+        if(result.val()) {
+          this.memos = result.val()
+        }
+      })
+  },
   methods: {
     logout: function(){
       firebase.auth().signOut()
@@ -49,6 +60,11 @@ export default {
       if(this.selectedIndex > 0) {
         this.selectedIndex--
       }
+    },
+    saveMemos: function() {
+      firebase.database()
+        .ref(`memos/${this.user.uid}`)
+        .set(this.memos);
     },
     preview: function() {
       return marked(this.memos[this.selectedIndex].markdown)
@@ -86,6 +102,9 @@ export default {
 }
 .addMemoBtn {
     margin-top: 20px;
+}
+.deleteMemoBtn {
+  margin: 10px;
 }
 .memoTitle {
   height: 1.5em;
