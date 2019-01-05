6cb032623b97bbd493a18659f087391e58996037
ichiroc <nobitahouse@gmail.com>
2019-01-05 13:17:42 +0900

    vue-router 導入

---
+++ src/views/Terms.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id='terms'>
+        <h1>利用規約</h1>
+        <p>変な文章入れちゃダメだよ</p>
+        <router-link :to="{name: 'top'}">TOPに戻る</router-link>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'terms'
+}
+</script>
